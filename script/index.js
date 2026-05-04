0a2f712d97019ffdee8f1b8161bb758061852543
jk-jumur <developer.jumur@gmail.com>
2026-05-04 23:42:33 +0600

    complete english janala vocabulary web app

--- script/index.js
+++ script/index.js
@@ -3,6 +3,12 @@ const createElements = (arr)=>{
       return(htmlElements.join(" "));
 }
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status)=>{
     if(status==true){ 
         document.getElementById("spinner").classList.remove("hidden");
@@ -107,7 +113,7 @@ const DisplayWordDetails = (word) =>{
                     <div class="text-2xl font-bold font-bangla text-[#18181B]">"${word.meaning? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation? word.pronunciation: "pronunciation পাওয়া যায়নি"}"</div>
                      <div class="flex justify-between items-center">
                     <button  onclick="loadWordDetail(${word.id})"  class="btn bg-[#1A91FF10]  hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                    <button class="btn bg-[#1A91FF10]  hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                    <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10]  hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
                 </div>
               `;
